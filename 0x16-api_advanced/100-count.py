68ae816d4b83278d5510f63d7f01d79518466f80
Maxidonx <johnmaxwell92@gmail.com>
2023-06-06 20:15:26 +0000

    Count

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,32 +1,61 @@
 #!/usr/bin/python3
+""" Count it! """
+from requests import get
 
-import requests
+REDDIT = "https://www.reddit.com/"
+HEADERS = {'user-agent': 'esw1229/0.0.1'}
 
-def count_words(subreddit, word_list):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {"User-Agent": "Custom User Agent"}
 
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        articles = data.get("data", {}).get("children", [])
+def count_words(subreddit, word_list, after="", word_dic={}):
+    """
+    Returns a list containing the titles of all hot articles for a
+    given subreddit. If no results are found for the given subreddit,
+    the function should return None.
+    """
+    if not word_dic:
+        for word in word_list:
+            word_dic[word] = 0
 
-        if not articles:
-            return
+    if after is None:
+        word_list = [[key, value] for key, value in word_dic.items()]
+        word_list = sorted(word_list, key=lambda x: (-x[1], x[0]))
+        for w in word_list:
+            if w[1]:
+                print("{}: {}".format(w[0].lower(), w[1]))
+        return None
 
-        title_list = [article["data"]["title"].lower() for article in articles]
-        word_count = {}
+    url = REDDIT + "r/{}/hot/.json".format(subreddit)
 
-        for title in title_list:
-            for word in word_list:
-                if word.lower() in title.split():
-                    word_count[word] = word_count.get(word, 0) + 1
+    params = {
+        'limit': 100,
+        'after': after
+    }
 
-        sorted_words = sorted(word_count.items(), key=lambda x: (-x[1], x[0]))
-        for word, count in sorted_words:
-            print(f"{word.lower()}: {count}")
-    else:
-        return
+    r = get(url, headers=HEADERS, params=params, allow_redirects=False)
 
-    # Recursive call to fetch more articles
-    count_words(subreddit, word_list)
+    if r.status_code != 200:
+        return None
+
+    try:
+        js = r.json()
+
+    except ValueError:
+        return None
+
+    try:
+
+        data = js.get("data")
+        after = data.get("after")
+        children = data.get("children")
+        for child in children:
+            post = child.get("data")
+            title = post.get("title")
+            lower = [s.lower() for s in title.split(' ')]
+
+            for w in word_list:
+                word_dic[w] += lower.count(w.lower())
+
+    except:
+        return None
+
+    count_words(subreddit, word_list, after, word_dic)
