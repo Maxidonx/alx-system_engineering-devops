c14ccd58f94b8b7a0cdc51af39932dbd0fcf6741
Maxidonx <johnmaxwell92@gmail.com>
2023-06-07 18:26:37 +0000

    Updated

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,18 @@
-# Fix typo in WordPress settings file
-exec { 'Fix typo in filename':
-  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
-  provider => shell,
+# Install Apache package
+package { 'apache2':
+  ensure => installed,
+}
+
+# Modify Apache configuration file
+file { '/etc/apache2/apache2.conf':
+  ensure  => present,
+  content => template('your_module/apache2.conf.erb'),
+  notify  => Service['apache2'],
+}
+
+# Restart Apache service
+service { 'apache2':
+  ensure  => running,
+  enable  => true,
+  require => Package['apache2'],
 }
